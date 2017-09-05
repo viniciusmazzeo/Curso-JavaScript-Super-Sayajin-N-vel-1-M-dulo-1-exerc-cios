6b0a9d25b7099a85c08bb7156e583910f5a1bbb6
viniciusmazzeo <viniciusmazzeo@gmail.com>
2017-09-04 23:12:35 -0300

    Exercicio aula 02

---
+++ aula_02/exercicioTeorema.js
@@ -0,0 +1,15 @@
+const add = ( y ) => ( x ) => x + y;
+const pow = ( y ) => ( x ) => Math.pow( x, y);
+const root = ( y ) => ( x ) => Math.pow( x, 1/y);
+
+const squareRoot = root( 2 );
+const elevacao = pow( 2 );
+
+const teoremaDePitagoras = ( b, c ) => {
+    
+    let a = add(elevacao( b ))( elevacao( c ))
+
+    return squareRoot( a );
+}
+
+console.log( teoremaDePitagoras( 9, 12 ) )
